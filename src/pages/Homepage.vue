ff98d5609f44ab3098c4cacd923fdef937e535ae
Mariz Melo <mm@xchema.com>
2020-05-09 01:57:48 -0700

    Updates - adding normalize.css - Chanding title - adding favicon and social icons - removing unused files

---
+++ src/pages/Homepage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="">
+    <h1>Hello world</h1>
+    hello world
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style scoped>
+
+</style>
