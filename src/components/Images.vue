1d8f098443521037dc59161ef7ec4a9fcce25702
Mariz Melo <mm@xchema.com>
2020-05-11 16:37:30 -0700

    Adding missing Images component from last commit

---
+++ src/components/Images.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="content.url" :width="content.width || '100%'" :alt="content.alt">
+</template>
+<script>
+export default {
+  name: "",
+  props: {
+    content: {}
+  },
+  data: () => ({})
+}
+</script>
+<style scoped>
+</style>
